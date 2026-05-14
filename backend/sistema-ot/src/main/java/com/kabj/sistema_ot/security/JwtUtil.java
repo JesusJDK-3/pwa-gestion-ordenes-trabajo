cfa3529fb87e2555c22079a351b40a4540236fe3
JDK <luciani29102003@gmail.com>
2026-05-14 17:30:53 -0500

    bad

--- backend/sistema-ot/src/main/java/com/kabj/sistema_ot/security/JwtUtil.java
+++ backend/sistema-ot/src/main/java/com/kabj/sistema_ot/security/JwtUtil.java
@@ -44,10 +44,10 @@ public class JwtUtil {
     }
 
     private Claims getClaims(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(getKey())
+        return Jwts.parser()
+                .verifyWith((javax.crypto.SecretKey) getKey())
                 .build()
-                .parseClaimsJws(token)
-                .getBody();
+                .parseSignedClaims(token)
+                .getPayload();
     }
 }
