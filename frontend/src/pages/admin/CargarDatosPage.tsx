a3a10e3de0ed766d18a52ac9bca7ddbb9dabb1b5
JDK <luciani29102003@gmail.com>
2026-06-26 15:57:35 -0500

    Revert "edit_text3"
    This reverts commit 2a80d5a99dff034c4e6dd29f3a36b18446307a87.

--- frontend/src/pages/admin/CargarDatosPage.tsx
+++ frontend/src/pages/admin/CargarDatosPage.tsx
@@ -8,7 +8,7 @@ export default function CargarDatosPage() {
           <p className="page-breadcrumb">Administrador · Datos maestros</p>
           <h1 className="page-title">Base geográfica VPA e Hidrantes</h1>
           <p className="page-subtitle">
-            Carga estos <strong>archivos</strong> . La cual brinda las coordenadas designadas para conectar las (<em>OT</em>) .
+            Carga estos<strong>archivos</strong> . La cual brinda las coordenadas designadas para conectar las (<em>OT</em>) .
           </p>
         </div>
       </div>
